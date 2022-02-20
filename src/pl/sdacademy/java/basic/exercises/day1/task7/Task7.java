79082785aea659d56989b8364a835427f3dc802b
mborak <m@borak.pl>
2022-02-20 09:39:19 +0100

    poprawka1

--- src/pl/sdacademy/java/basic/exercises/day1/task7/Task7.java
+++ src/pl/sdacademy/java/basic/exercises/day1/task7/Task7.java
@@ -3,16 +3,39 @@ package pl.sdacademy.java.basic.exercises.day1.task7;
 import java.util.Scanner;
 
 public class Task7 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String decision;
         do {
             System.out.println("Calculating...");
-            System.out.println("New calculation or 'Stop'");
+            // poibieramy dwie liczby i operator od usera
+            //fun()
+
+            // pytamy usera czy chce kontynuować
+            // pobieramy odpowiedz od usera
+            System.out.print("New calculation or 'Stop' ");
             decision = scanner.nextLine();
-        } while (!("Stop".equalsIgnoreCase(decision))); // (!(decision.equals("Stop"))
+        } while(!("Stop".equalsIgnoreCase(decision)));
     }
-    
+
+    private static void fun(float firstDigit, float secondDigit, String operator) {
+        switch (operator) {
+            case "+":
+                // logika
+                break;
+            case "-":
+                // logika
+                break;
+            case "*":
+                // logika
+                break;
+            case "/":
+                // logika
+                break;
+            default:
+                System.out.println("Wrong Operator");
+        }
     }
 }
