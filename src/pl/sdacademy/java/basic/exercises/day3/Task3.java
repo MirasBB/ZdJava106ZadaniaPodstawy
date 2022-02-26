cb8affa68e978817dee3f8e80b67dd26ca0892fd
mborak <m@borak.pl>
2022-02-26 11:07:34 +0100

    day3 Task 3

---
+++ src/pl/sdacademy/java/basic/exercises/day3/Task3.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.java.basic.exercises.day3;
+
+import java.util.regex.Pattern;
+
+public class Task3 {
+//    private static final String NUMBER_REGEX = "-?[0-9]+(,[0-9]+)?";
+private static final String NUMBER_REGEX = "-?\\d+(,\\d+)?";
+
+    public static void main(String[] args) {
+        String number = "-12341234";
+        System.out.println("If digit " + number + " has correct format: " + isCorrectNumber(number));
+
+    }
+
+    private static boolean isCorrectNumber(String number){
+        Pattern pattern = Pattern.compile(NUMBER_REGEX);
+        return pattern.matcher(number).matches();
+    }
+}
